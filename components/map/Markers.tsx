2ac956e004a1439d3dd16263c68b7d08cfcfc4c6
SatooRu65536 <87361015+SatooRu65536@users.noreply.github.com>
2024-01-19 15:08:29 +0900

    improve: コードを修正

--- components/map/Markers.tsx
+++ components/map/Markers.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { MARKER_COLOR_IMG, MARKER_SELECT_COLOR } from "@/const/marker";
+import { MARKER_SELECT_COLOR } from "@/const/marker";
 import { CastleMarker } from "@/types/map";
-import { Icon, Marker as Mark, icon } from "leaflet";
-import { useEffect, useMemo, useRef, useState } from "react";
-import { Marker, Popup, useMapEvents } from "react-leaflet";
+import { icon } from "leaflet";
+import { Marker, Popup } from "react-leaflet";
 
 type Props = {
   marker: CastleMarker;
@@ -13,31 +12,16 @@ type Props = {
 
 export default function Markers(props: Props) {
   const { marker, isSelected } = props;
-  const markerRef = useRef<Mark>(null);
-  const [markerIcon, setMarkerIcon] = useState<Icon>(
-    icon({
-      iconUrl: isSelected
-        ? MARKER_SELECT_COLOR.img.src
-        : MARKER_COLOR_IMG[0].src,
-      iconSize: [40, 40],
-      iconAnchor: [20, 40],
-      popupAnchor: [0, -40],
-    }),
-  );
 
-  useEffect(() => {
-    const markerIconSnap = icon({
-      iconUrl: marker.img.src,
-      iconSize: [40, 40],
-      iconAnchor: [20, 40],
-      popupAnchor: [0, -40],
-    });
-
-    setMarkerIcon(markerIconSnap);
-  }, [marker.img.src]);
+  const markerIcon = icon({
+    iconUrl: isSelected ? MARKER_SELECT_COLOR.img.src : marker.img.src,
+    iconSize: [40, 40],
+    iconAnchor: [20, 20],
+    popupAnchor: [0, -40],
+  });
 
   return (
-    <Marker ref={markerRef} position={marker.coordinates} icon={markerIcon}>
+    <Marker position={marker.coordinates} icon={markerIcon}>
       <Popup>
         <a href={`/castle/${marker.id}`}>{marker.name}</a>
       </Popup>
